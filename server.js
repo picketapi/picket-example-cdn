aaefba6a0506f4a45057253ec04d2a049ff5db4b
Devin Stein <devstein@seas.upenn.edu>
2022-07-20 13:20:02 -0700

    Run prettier, reference port in message, use latest picket

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const app = express();
+const port = 8080;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/js-cdn-example.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/js-cdn-example.html");
 });
 
-app.get('/gated-content', function(req, res){
-    res.sendFile(__dirname + '/public/gated-content-simple.html');
+app.get("/gated-content", function (req, res) {
+  res.sendFile(__dirname + "/public/gated-content-simple.html");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(
+    `Example app listening on port ${port}. Open http://localhost:${port}/ in your browser.`
+  );
+});
+
